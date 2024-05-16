51b4121de198f3cd351898a0d868b2cd1247642d
Aditya Bhanu Garg <adityabhanugarg@gmail.com>
2024-05-16 14:36:30 +0530

    Node-activity

---
+++ app.js
@@ -0,0 +1,5 @@
+
+const _ = require('lodash')
+const items=[1,[2,[34,[89,98,7789]]]]
+console.log(_.flattenDeep(items)) 
+
---
+++ test.js
@@ -0,0 +1,5 @@
+const name1='Francisco'
+const name2='Mcqueen'
+const name3='Engineer'
+
+module.exports={name1,name2}
